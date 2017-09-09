94f4c5044b6ca0cccdecc176cb43732bba5cd859
Michael Munn <michael.munn@gmail.com>
2017-09-08 18:35:29 -0700

    made pytyon lamda ready

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -3,6 +3,7 @@ import StringIO
 import zipfile
 import mimetypes
 
+
 s3 = boto3.resource('s3')
 
 build_bucket = s3.Bucket('portfoliobuild')
@@ -13,6 +14,7 @@ build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
 with zipfile.ZipFile(portfolio_zip) as myzip:
     for nm in myzip.namelist():
+        print nm
         obj = myzip.open(nm)
         portfolio_bucket.upload_fileobj(obj,nm,
             ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
